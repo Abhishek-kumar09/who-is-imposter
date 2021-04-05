86c0261d351f82f21302053acb85f5d1ab6f9ab9
Siddesh Sambasivam <plutocrat45@gmail.com>
2021-04-06 02:42:44 +0530

    added chrome storage for twitter

---
+++ background.js
@@ -0,0 +1,23 @@
+function initialize_defaults() {
+    chrome.storage.sync.get("defaults_initialized", function(obj) {
+        if (obj.defaults_initialized)
+            return;
+
+        chrome.storage.sync.set({"twitter_enabled":true});
+    })
+}
+
+initialize_defaults();
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+    //Called when the user clicks on the browser action.
+    bookmarklet();
+});
+
+function bookmarklet() {
+    console.log('Tracking')
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        chrome.tabs.sendMessage(tabs[0].id, {'message': 'bookmarklet'});
+    });
+}
+
