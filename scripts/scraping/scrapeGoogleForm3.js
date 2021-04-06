e65f654412be67438122f3de9edcbfceed961f87
ravisha7feb <ravisha7feb@gmail.com>
2021-04-06 11:42:20 +0530

    modified the code

--- scripts/scraping/scrapeGoogleForm3.js
+++ scripts/scraping/scrapeGoogleForm3.js
@@ -16,16 +16,36 @@ async function scrapeProduct(url){
         return formTag;
 
     })
-
-    
+    const numberOfFields = await page.evaluate( () => {
+        const num = (String(document.querySelector("form > div.freebirdFormviewerViewFormCard.exportFormCard > div > div.freebirdFormviewerViewItemList")).length) - 3
+        return num;
+    })
    
+    const field1 = await page.evaluate( () => {
+        const label1 = String(document.querySelector("form > div.freebirdFormviewerViewFormCard.exportFormCard > div > div.freebirdFormviewerViewItemList > div:nth-child(1)").innerText)
+        const len = label1.length;
+        return label1.substring(0, (len - 11));
+    }) 
+
     await browser.close();
 
-    console.log(titleData, '\n');
-    console.log(fields);
 
-    //return data;
-
+    console.log('\n');
+    console.log("Title: ", titleData, '\n');
+    console.log("Number of Fields: ", numberOfFields, '\n');
+    console.log("Field-1: ", field1, '\n');
+    
+    /*for(var i = 1; i <= numberOfFields; i++){
+        var field = await page.evaluate( () => {
+            var arg = "form > div.freebirdFormviewerViewFormCard.exportFormCard > div > div.freebirdFormviewerViewItemList > div:nth-child(" + String(i) +")"
+            var label = document.querySelector(arg).innerText
+            return label;
+        })
+        await browser.close();
+        console.log(field, '\n');
+    }*/
+    
+    
 }
 
 scrapeProduct('https://docs.google.com/forms/d/e/1FAIpQLSfv7eR5oZ-4YJ3Nwb_e4iUCuZRyrJCF7kNYqlPC6bQA-HtlhQ/viewform');
